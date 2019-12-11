b9792844a1d396e92e0cec15a95c5b82007d5f6e
luisantoniocrag <tricubeart@gmail.com>
2019-12-11 10:00:00 +0530

    Dex contract: Token Registry

---
+++ packages/contracts/contracts/Dex.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT 
+
+pragma solidity 0.6.3;
+
+contract DEX {
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    mapping(bytes32 => Token) public tokens;
+    bytes32[] public tokenList;
+
+    address public admin;
+
+    constructor() public {
+        admin = msg.sender;
+    }
+
+    function addToken(
+        bytes32 ticker,
+        address tokenAddress) 
+        onlyAdmin()
+        external {
+        tokens[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only admin");
+        _;
+    }
+
+}
